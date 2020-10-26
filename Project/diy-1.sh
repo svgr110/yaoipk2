432a66049d2ce4fcc65defd95bf3992ad137e9a8
svgr110 <70746911+svgr110@users.noreply.github.com>
2020-10-26 15:26:11 +0800

    Update diy-1.sh

--- Project/diy-1.sh
+++ Project/diy-1.sh
@@ -4,7 +4,7 @@
 # 说明：
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
 
-
+git clone https://github.com/prcxu/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 #源码里面几乎囊括了所有插件，所以基本不需要添加插件了
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
