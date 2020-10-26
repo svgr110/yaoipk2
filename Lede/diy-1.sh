93ca2760ce449efb9edae3dac695d7f0393f20e7
svgr110 <70746911+svgr110@users.noreply.github.com>
2020-10-26 14:06:01 +0800

    Update diy-1.sh

--- Lede/diy-1.sh
+++ Lede/diy-1.sh
@@ -16,7 +16,7 @@ git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-
 git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-git clone https://github.com/user1121114685/koolproxyR/koolproxyR/package/lean/luci-app-koolproxyR
+
 git clone https://github.com/user1121114685/koolproxyR/package/lean/luci-app-koolproxyR
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 
